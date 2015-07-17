d49ce1178e1bb93bb78124a7467878108f23c35d
robert fine <robertfine4631@icloud.com>
2015-07-17 15:43:26 -0400

    finished translate

--- exercises.js
+++ exercises.js
@@ -43,9 +43,27 @@ console.log("enter a single character to determine if it is a vowel");
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function rovarspraket(phrase){
+//on this one, stack overflow helped out a whole lot
+var translation = [];
 
+function rovarspraket(phrase){
+    var translate = phrase.split('');
+
+    translate.forEach( function (letter){
+        if ("aeiou ".indexOf(letter) < 0) {
+            translation.push(letter + 'o' + letter);
+        }
+        else if ("aeiou ".indexOf(letter) >= 0) {
+            translation.push(letter);
+        }
+    });
+
+    return translation;
 }
+
+console.log(rovarspraket);
+console.log('function takes a string or phrase');
+
 
 // ---------------------
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
